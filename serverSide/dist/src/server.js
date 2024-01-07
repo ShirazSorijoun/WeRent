958c30b301ec8eafa10d0825f9638f24f5d93950
Shiraz <shirazsorijoun@gmail.com>
2024-01-07 20:47:15 +0200

    Update documentation

--- serverSide/dist/src/server.js
+++ serverSide/dist/src/server.js
@@ -4,10 +4,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
 (0, app_1.default)().then((app) => {
+    const options = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "Web Dev - 'WeRent' 2023 REST API",
+                version: "1.0.0",
+                description: "REST server including authentication using JWT",
+            },
+            servers: [{ url: "http://localhost:3000", },],
+        },
+        apis: ["./src/routes/*.ts"],
+    };
+    const specs = (0, swagger_jsdoc_1.default)(options);
+    app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(specs));
     const port = process.env.PORT;
     app.listen(port, () => {
         console.log(`Listening on port ${port}`);
     });
+    /*
+    if (process.env.NODE_ENV !== "production") {
+      console.log("Development mode");
+      http.createServer(app).listen(process.env.PORT);
+    }
+    */
 });
 //# sourceMappingURL=server.js.map
--- serverSide/src/server.ts
+++ serverSide/src/server.ts
@@ -1,8 +1,38 @@
 import initApp from "./app";
+import swaggerUI from "swagger-ui-express"
+import swaggerJsDoc from "swagger-jsdoc"
+import http from "http";
+
+
+
 
 initApp().then((app)=>{
+
+  const options = {
+    definition: {
+    openapi: "3.0.0",
+    info: {
+    title: "'WeRent' 2023 REST API",
+    version: "1.0.1",
+    description: "REST server including authentication using JWT and refresh token",
+    },
+    servers: [{url: "http://localhost:3000",},],
+    },
+    apis: ["./src/routes/*.ts"],
+    };
+    const specs = swaggerJsDoc(options);
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
     const port = process.env.PORT;
     app.listen(port , ()=>{
        console.log(`Listening on port ${port}`);
-  });
+    });
+
+    /*
+    if (process.env.NODE_ENV !== "production") {
+      console.log("Development mode");
+      http.createServer(app).listen(process.env.PORT);
+    }
+    */
+
 });

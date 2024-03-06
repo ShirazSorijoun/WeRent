75c6de79892c544d05e003f7a8d86c965e95e72d
Shiraz <shirazsorijoun@gmail.com>
2024-03-06 14:30:54 +0200

    prod

--- serverSide/src/server.ts
+++ serverSide/src/server.ts
@@ -2,7 +2,7 @@ import initApp from "./app";
 import swaggerUI from "swagger-ui-express"
 import swaggerJsDoc from "swagger-jsdoc"
 //import https from 'https';
-//import http from 'http';
+import http from 'http';
 //import fs from 'fs';
 
 
@@ -25,17 +25,19 @@ initApp().then((app)=>{
     const specs = swaggerJsDoc(options);
     app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-    const port = process.env.PORT;
-    app.listen(port , ()=>{
-       console.log(`Listening on port ${port}`);
-    });
+  
+    //const port = process.env.PORT;
+    //app.listen(port , ()=>{
+       //console.log(`Listening on port ${port}`);
+    //});
 
-    /*
+    
     if (process.env.NODE_ENV !== "production") {
       console.log("Development mode");
+      console.log(`Listening on port ${process.env.PORT}`);
       http.createServer(app).listen(process.env.PORT);
     }
-    */
+    
 
     /*
     if (process.env.NODE_ENV !== 'production') {

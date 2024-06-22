6d68ce70fd939c7653453d9ba8cb4da7a50119ec
galronen10 <galronen10@gmail.com>
2024-06-22 20:39:02 +0300

    improve code of start

--- serverSide/src/app.ts
+++ serverSide/src/app.ts
@@ -3,11 +3,28 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsDoc from 'swagger-jsdoc';
 import authRoute from './routes/auth_route';
 import userRoute from './routes/user_route';
 import apartmentRoute from './routes/apartment_route';
 import userReviewRoute from './routes/user_review_route';
 import fileRoute from './routes/file_route';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: "'WeRent' 2023 REST API",
+      version: '1.0.1',
+      description:
+        'REST server including authentication using JWT and refresh token',
+    },
+    servers: [{ url: `http://localhost:3000` }],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+const specs = swaggerJsDoc(options);
 
 dotenv.config();
 
@@ -17,13 +34,16 @@ const initApp = (): Promise<Express> => {
     db.on('error', (error) => {
       console.error(error);
     });
-    db.once('open', () => console.log('connected to the database'));
+    db.once('open', () => {
+      console.log('connected to the database');
+    });
+
     const url = process.env.DATABASE_URL;
     mongoose.connect(url!).then(() => {
       const app = express();
 
-      app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }));
-      app.use(bodyParser.json());
+      app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+      app.use(bodyParser.json({ limit: '50mb' }));
 
       // Enable CORS for all routes
       app.use(cors());
@@ -35,6 +55,7 @@ const initApp = (): Promise<Express> => {
       app.use('/file', fileRoute);
       app.use('/public', express.static('public'));
 
+      app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
       app.use('/assets', express.static('static/assets'));
       app.use('*', (req, res) => {
         res.sendFile('index.html', { root: 'static/' });

6d73f1b1f0ec6e539d995757d41e0f4ad43b1128
Shay Meiron <smeiron@quotient.com>
2024-07-22 20:38:40 +0300

    add _id

--- serverSide/src/controllers/apartment_controller.ts
+++ serverSide/src/controllers/apartment_controller.ts
@@ -210,7 +210,7 @@ const getMatchesByApartmentId = async (req: Request, res: Response): Promise<voi
 
     const matches = await Match.find({ apartment: apartmentId }).populate({
       path: 'user',
-      select: 'name email',
+      select: 'name email _id',
     });
 
     if (!matches || matches.length === 0) {

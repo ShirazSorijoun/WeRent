9d07a1d86a969c4138d0a486ceadb775194075cb
Shani Yaish <yaish_shani@walla.com>
2024-07-27 13:24:43 +0300

    add and fix forms

--- serverSide/src/controllers/tenant_form_controller.ts
+++ serverSide/src/controllers/tenant_form_controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
-import TenantForm from '../models/tenant_form';
+import TenantFormInitial from '../models/tenant_form_initial';
+import TenantQuestionnaireQuaterly from '../models/tenant_form_quarterly';
 
 interface AuthRequest extends Request {
   locals?: {
@@ -7,7 +8,7 @@ interface AuthRequest extends Request {
   };
 }
 
-const createTenantForm = async (
+const createTenantFormInitial = async (
   req: AuthRequest,
   res: Response,
 ): Promise<void> => {
@@ -15,7 +16,7 @@ const createTenantForm = async (
     const { tenantForm } = req.body;
     console.log('tenantForm', tenantForm);
     tenantForm.owner = req.locals.currentUserId;
-    const createdForm = await TenantForm.create(tenantForm);
+    const createdForm = await TenantFormInitial.create(tenantForm);
 
     res.status(201).json(createdForm);
   } catch (error) {
@@ -24,6 +25,60 @@ const createTenantForm = async (
   }
 };
 
+const getTenantFormInitialByOwnerId = async (req: Request, res: Response): Promise<void> =>{
+  try {
+    const { ownerId } = req.params;
+    console.log('ownerId', ownerId);
+    const tenantForm = await TenantFormInitial.findOne({ owner: ownerId });
+    console.log('tenantForm', tenantForm);
+    if (tenantForm) {
+      console.log('tenantForm', tenantForm);
+      res.status(200).json(tenantForm);
+    } else {
+      res.status(404).json({ message: 'Tenant form not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+const createTenantFormQuarterly = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { tenantForm } = req.body;
+    console.log('tenantForm', tenantForm);
+    tenantForm.owner = req.locals.currentUserId;
+    const createdForm = await TenantQuestionnaireQuaterly.create(tenantForm);
+
+    res.status(201).json(createdForm);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send('Something went wrong -> createTenantForm');
+  }
+};
+
+const getTenantFormQuarterlyByOwnerId = async (req: Request, res: Response): Promise<void> =>{
+  try {
+    const { ownerId } = req.params;
+    console.log('ownerId', ownerId);
+    const tenantForm = await TenantQuestionnaireQuaterly.findOne({ owner: ownerId });
+    console.log('tenantForm', tenantForm);
+    if (tenantForm) {
+      console.log('tenantForm', tenantForm);
+      res.status(200).json(tenantForm);
+    } else {
+      res.status(404).json({ message: 'Tenant form not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
 export default {
-  createTenantForm,
+  createTenantFormInitial,
+  getTenantFormInitialByOwnerId,
+  createTenantFormQuarterly,
+  getTenantFormQuarterlyByOwnerId,
 };

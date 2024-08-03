b4c3444c9ef8b152e8822d7ff9a6b9c8a10598e1
Shani Yaish <yaish_shani@walla.com>
2024-08-03 17:57:43 +0300

    fix google register

---
+++ serverSide/src/routes/tenant_form_route.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import AuthMiddleware from '../common/auth_middleware';
+import TenantFormController from '../controllers/tenant_form_controller';
+
+
+const router = express.Router();
+
+router.post('/create',AuthMiddleware, TenantFormController.createTenantForm);
+
+export default router;
